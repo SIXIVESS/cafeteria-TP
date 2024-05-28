49e21906ad60cae40a0945fff0b2794223c92530
SIXIVESS <mettatonbb@gmail.com>
2024-05-28 02:44:05 -0700

    Design finished

---
+++ js/countdown.js
@@ -0,0 +1,17 @@
+const targetDate = new Date("July 20, 2025 19:00:00").getTime();
+
+setInterval(() => {
+  const now = new Date().getTime();
+
+  const remainingTime = targetDate - now;
+
+  const days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+}, 1000);
